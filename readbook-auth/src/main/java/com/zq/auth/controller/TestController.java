2eb398fc538547627ca1e2a4b44ef73b4538a811
zhouqi <qi.zhou@njyjxr.com>
2025-08-01 15:00:55 +0800

    日志、日期格式

--- readbook-auth/src/main/java/com/zq/auth/controller/TestController.java
+++ readbook-auth/src/main/java/com/zq/auth/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.zq.auth.controller;
 
 import com.zq.auth.pojo.User;
+import com.zq.auth.pojo.UserDO;
 import com.zq.framework.common.response.Response;
 import com.zq.framework.log.aspect.ApiOperationLog;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -18,12 +21,9 @@ public class TestController {
     }
 
 
-    @GetMapping("/test2")
+    @PostMapping("/test2")
     @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2() {
-        return Response.success(User.builder()
-                .nickName("zq")
-                .createTime(LocalDateTime.now())
-                .build());
+    public Response<User> test2(@RequestBody User user) {
+        return Response.success(user);
     }
 }
---
+++ readbook-auth/src/main/java/com/zq/auth/mapper/UserDOMapper.java
@@ -0,0 +1,17 @@
+package com.zq.auth.mapper;
+
+import com.zq.auth.pojo.UserDO;
+
+public interface UserDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserDO record);
+
+    int insertSelective(UserDO record);
+
+    UserDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserDO record);
+
+    int updateByPrimaryKey(UserDO record);
+}
