bd2c242123720f231579b34755325eec33d4b2b4
zhouqi <taishuailezhouqi@163.com>
2025-08-02 16:16:43 +0800

    登陆

---
+++ readbook-auth/src/main/java/com/zq/auth/pojo/User.java
@@ -0,0 +1,50 @@
+package com.zq.auth.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_user")
+public class User {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private String readBookId;
+
+    private String password;
+
+    private String nickname;
+
+    private String avatar;
+
+    private Date birthday;
+
+    private String backgroundImg;
+
+    private String phone;
+
+    private Byte sex;
+
+    private Byte status;
+
+    private String introduction;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private int isDeleted;
+
+}
